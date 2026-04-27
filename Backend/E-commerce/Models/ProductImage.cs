f4ac7b185a2bc86382a5eeb0dbae54d2a929747f
TruongVan-25 <van.truong.cit23@eiu.edu.vn>
2026-04-28 00:16:19 +0700

    finish model for ProductImage and ProductVariant

--- Backend/E-commerce/Models/ProductImage.cs
+++ Backend/E-commerce/Models/ProductImage.cs
@@ -1,6 +1,18 @@
-﻿namespace E_commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_commerce.Models
 {
     public class ProductImage
     {
+        [Key]
+        public Guid ProductImageId {set; get;} = Guid.NewGuid();
+
+        public Guid ProductId {get; set;}
+        [ForeignKey("ProductId")]
+        public Product? Product {get; set;}
+
+        [Required]
+        public string ImageUrl {get; set;} = null!;
     }
 }
--- Backend/E-commerce/Models/ProductVariant.cs
+++ Backend/E-commerce/Models/ProductVariant.cs
@@ -1,6 +1,25 @@
-﻿namespace E_commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_commerce.Models
 {
     public class ProductVariant
     {
+        [Key]
+        public Guid ProductVariantId {get; set;} = Guid.NewGuid();
+        
+        public Guid ProductId {get; set;}
+        [ForeignKey("ProductId")]
+        public virtual Product? Product {get; set;}
+
+        [Required(ErrorMessage="Product variant name is required!")]
+        public string Name {set; get;} = null!;
+
+        [Required(ErrorMessage="Product variant price is required!")]
+        [Column(TypeName="decimal(18, 2)")]
+        public decimal Price {set; get;}
+
+        [Required(ErrorMessage="Product variant quantity is required!")]
+        public int Quantity {set; get;}
     }
 }
