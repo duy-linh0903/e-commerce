2464c64f4c68a6d9f03e42725cded65f7956f21f
Nguyen Chi Dai <dai.nguyen.cit23@eiu.edu.vn>
2026-04-28 23:12:33 +0700

    Add: Brand Database

--- Backend/E-commerce/Models/Brand.cs
+++ Backend/E-commerce/Models/Brand.cs
@@ -5,5 +5,13 @@ namespace E_commerce.Models
 {
     public class Brand
     {
+        [Key] // khóa chính
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } // tên brand 
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+        // 1 Brand -> n Product
     }
 }
