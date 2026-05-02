6c178806671c952197eb3ff57d5e1b899810468a
Trương Châu Phước Lộc <139109932+Yukanya@users.noreply.github.com>
2026-05-02 23:09:02 +0700

    Add: Properties to Role model

--- Backend/E-commerce/Models/Role.cs
+++ Backend/E-commerce/Models/Role.cs
@@ -1,9 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace E_commerce.Models
 {
     public class Role
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required(ErrorMessage = "Role name is required.")]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<User> Users { get; set; } = new List<User>();
     }
 }
